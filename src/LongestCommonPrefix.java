8de5037c7068c7649bac2eda43c99c7654bf1f26
Güner KALAN <gunerkalan@gmail.com>
2023-03-01 10:21:01 +0300

    palindrome Number

---
+++ src/LongestCommonPrefix.java
@@ -0,0 +1,31 @@
+public class LongestCommonPrefix {
+
+    public static void main(String[] args) {
+
+        LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+        String[] st = {"flower", "flow", "flight"};
+        String result = longestCommonPrefix.longestCommonPrefix(st);
+        System.out.println(result);
+
+    }
+
+    public String longestCommonPrefix(String[] strs){
+        if(strs==null || strs.length==0){
+        return "";
+    }
+
+    String prefix = strs[0];
+
+        for(int i=1; i< strs.length; i++){
+            while (strs[i].indexOf(prefix)!=0){
+                prefix = prefix.substring(0, prefix.length()-1);
+                if(prefix.isEmpty()){
+                    return "";
+                }
+            }
+        }
+        return prefix;
+
+    }
+
+}
